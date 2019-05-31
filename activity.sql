2e253d50d3b879874d109b7d203be93b3b5d4bec
Meredith2333 <49987811+Meredith2333@users.noreply.github.com>
2019-05-31 08:48:31 +0800

    Add files via upload

--- activity.sql
+++ activity.sql
@@ -9,4 +9,19 @@ create table activity(
   id int(11) NOT NULL UNIQUE,
   limits int(11) NOT NULL,
   lname varchar(255) NOT NULL,
+  nane varchar(255) NOT NULL,
+  ooid int(11) NOT NULL,
+  oname varchar(255) NOT NULL,
+  pubtime date NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE = InnoDB DEFULT CHARSET = utf8;
+
+create table studeng( 
+  id int(11) NOT NULL NUIQUE,
+  name varchar(255) NOT NULL,
+  password int(11) NOT NULL,
+  credits int(11) NOT NULL,
+  phone varchar(255) NOT NULL,
+  address varchar(244) NOT NULL,
+  PRIMARY KRY(id)
 )
